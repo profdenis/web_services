dc3db97c54f5edbe62a61be81f9cf49e3684fef9
profdenis <denis@profdenis.com>
2023-01-27 10:23:55 -0500

    added in-class example project web_services_app2

---
+++ code/inclass/web_services_app2/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify
+from exercises_functions import is_even, is_palindrome, reverse, occurrences
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!!!!!!!!!!!!!!!!!!!'
+
+
+# localhost:5000/is-even/23
+# x = 23
+@app.route('/is-even/<int:x>')
+def is_even_route(x):
+    return jsonify(is_even(x))
+
+
+@app.route('/is-palindrome/<string:s>')
+def is_palindrome_route(s):
+    return jsonify({'function': 'is_palindrome',
+                    'parameters': [s],
+                    'result': is_palindrome(s)})
+
+
+@app.route('/reverse/<string:s>')
+def reverse_route(s):
+    return jsonify({'function': 'reverse',
+                    'parameters': [s],
+                    'result': reverse(s)})
+
+
+@app.route('/occurrences/<string:target_char>/<string:s>')
+def occurrences_route(target_char, s):
+    return jsonify({'function': 'occurrences',
+                    'parameters': [target_char, s],
+                    'result': occurrences(target_char, s)})
+
+
+if __name__ == '__main__':
+    app.run()
