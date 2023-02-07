80439124d9af6c25cf5eb7895606e667f456ed45
profdenis <denis@profdenis.com>
2023-02-07 08:41:23 -0500

    updates 06_flask_mvc

--- code/06_flask_mvc/app.py
+++ code/06_flask_mvc/app.py
@@ -1,36 +1,73 @@
-from flask import Flask, render_template
+import json
+
+from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+import jsonpickle
 
 app = Flask(__name__)
 app.secret_key = 'allo'
 app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///contacts.sqlite'
 db = SQLAlchemy(app)
 
-from models import Contact, Call
+from models import Contact, Call, ContactEncoder
+
+app.json_encoder = ContactEncoder
 
 
-@app.route('/contact/')
+@app.route('/contacts')
 def all_contacts():
     contacts = Contact.query.all()
-    return render_template('contacts.html', contacts=contacts)
+    # return json.loads(jsonpickle.encode(contacts))
+    return jsonify(contacts)
 
 
-@app.route('/contact/<int:contact_id>')
+@app.route('/contacts/<int:contact_id>')
 def contact_by_id(contact_id):
     contact = Contact.query.get_or_404(contact_id)
-    return render_template('contact.html', contact=contact)
+    return jsonify(contact)
 
 
-@app.route('/call/')
+@app.route('/calls')
 def all_calls():
     calls = Call.query.all()
-    return render_template('calls.html', calls=calls)
+    # return json.loads(jsonpickle.encode(calls))
+    return jsonify(calls)
 
 
-@app.route('/call/<int:call_id>')
+@app.route('/calls/<int:call_id>')
 def call_by_id(call_id):
     call = Call.query.get_or_404(call_id)
-    return render_template('call.html', call=call)
+    return jsonify(call)
+
+
+@app.errorhandler(403)
+def forbidden(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'forbidden', 'code': 403})
+        response.status_code = 403
+        return response
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'not found', 'code': 404})
+        response.status_code = 404
+        return response
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'internal server error', 'code': 500})
+        response.status_code = 500
+        return response
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
--- code/06_flask_mvc/models.py
+++ code/06_flask_mvc/models.py
@@ -1,5 +1,26 @@
+import json
+
 from app import db
 import datetime
+
+
+class ContactEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, Contact):
+            return {'contact_id': o.contact_id,
+                    'name': o.name,
+                    'email': o.email,
+                    'phone': o.phone,
+                    'address': o.address
+                    }
+        elif isinstance(o, Call):
+            return {'call_id': o.call_id,
+                    'phone': o.phone,
+                    'datetime': str(o.datetime),
+                    'contact_id': o.contact_id
+                    }
+        else:
+            return super().default(o)
 
 
 class Contact(db.Model):
@@ -14,6 +35,9 @@ class Contact(db.Model):
     def __repr__(self):
         return f"<Contact {self.contact_id}: {self.name}>"
 
+    def to_dict(self):
+        return __dict__
+
 
 class Call(db.Model):
     __tablename__ = 'call'
@@ -24,3 +48,11 @@ class Call(db.Model):
 
     def __repr__(self):
         return f"<Call {self.call_id}: {self.phone} {self.contact_id}>"
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        del state['_sa_instance_state']
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
